c7f3b6c22925fe41416e8f15b672ef5c2f41de70
Desmond Kramer <destkramers@gmail.com>
2021-04-15 12:04:17 +0100

    Add sigmoid functions, and forward propagation

--- nueral_net.py
+++ nueral_net.py
@@ -1,6 +1,15 @@
 import numpy as np
 
 
+
+# Returns sigmoid
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def sigmoid_deriv(x):
+    return x*(1-x)
+
+
 class NueralNet:
     # param x: input array
     # param y: output array
@@ -10,3 +19,7 @@ class NueralNet:
         self.syn1 = np.random.rand(4,1) 
         self.y = y
         self.output = np.zeros(self.y.shape)
+
+    def f_propagation(self):
+        self.layer0 = sigmoid(np.dot(self.input, self.syn0))
+        self.output = sigmoid(np.dot(self.layer0, self.syn1))
